db95bf3f9fd27315e9613d461d2f4d448d34e624
Cmorgan21 <132255771+Cmorgan21@users.noreply.github.com>
2023-09-09 01:19:49 +0100

    take user name and score and store in object

---
+++ assets/js/highscore.js
@@ -0,0 +1,7 @@
+// jshint esversion: 6
+
+
+// stores username and highscore into a variable in new javascript page
+const userName = localStorage.getItem("name");
+const score = localStorage.getItem("HighScore");
+let scoreSystem = [];
