0677be18356ab41addaae42cf0f2c8ede979665c
Cmorgan21 <132255771+Cmorgan21@users.noreply.github.com>
2023-09-09 01:20:46 +0100

    unstringify data to allow it to be used

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -5,3 +5,12 @@
 const userName = localStorage.getItem("name");
 const score = localStorage.getItem("HighScore");
 let scoreSystem = [];
+
+/* 
+allows all objects to be stored into an array where they can all be accessed
+if there is data set with the variable scoreSystem it will unstringy the data allowing it to be accessed
+*/ 
+if (localStorage.getItem("scoreSystem")) {
+    scoreSystem = JSON.parse(localStorage.getItem("scoreSystem"));
+    console.log("scoreSystem:", scoreSystem);
+}
