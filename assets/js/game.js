b9458619c0ea3eea273f6caa0d29b0802b16cd4f
Cmorgan21 <132255771+Cmorgan21@users.noreply.github.com>
2023-09-09 01:04:49 +0100

    add function get name from form and remove form after

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,4 +14,24 @@ document.addEventListener("DOMContentLoaded", function(){
     gameNavigation.classList.add("game-container-transition");
     // allows all content to appear as content is loaded and a class that has a transition effect is added
   });
+
+  // When form is submitted this will hide the form and display the game board
+function startPage () {
+    let form = document.getElementById("name-form");
+    let gameContainer = document.getElementById("game-container");
+    let name = document.getElementById("name");
+    if (name.value === "") {
+      alert("Please enter a name");
+    } else {
+      form.style.display = "none";
+      gameContainer.classList.remove("hide");
+      form.classList.add("hide");
+      console.log("startPage Function was executed");
+      /* 
+      console.logging this allows me to ensure the function was executed
+      call the addName function once everything has been complete
+      */
+      addName();
+    }
+  }
   
